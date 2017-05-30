a308fc89d3ee9e8b5d96692a07a4670fe7dbaa68
Joe Lanman <joelanman@gmail.com>
2017-05-30 19:09:15 +0100

    fixes

--- app/routes.js
+++ app/routes.js
@@ -1,161 +1,141 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request-promise');
-var querystring = require('querystring');
+var express = require('express')
+var router = express.Router()
+var request = require('request-promise')
+var querystring = require('querystring')
 
-var numberToCommas = function (input){
-    return input.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+var numberToCommas = function (input) {
+  return input.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
 
-var log = function(data){
-  console.log(JSON.stringify(data, null, '  '));
+var log = function (data) {
+  console.log(JSON.stringify(data, null, '  '))
 }
 
 router.get('/', function (req, res) {
-
-  res.render('index');
-
-});
-
+  res.render('index')
+})
 
 // Example routes - feel free to delete these
 
 // Passing data into a page
 
 router.get('/examples/template-data', function (req, res) {
-
-  res.render('examples/template-data', { 'name' : 'Foo' });
-
-});
+  res.render('examples/template-data', { 'name': 'Foo' })
+})
 
 // Branching
 
 router.get('/examples/over-18', function (req, res) {
-
   // get the answer from the query string (eg. ?over18=false)
-  var over18 = req.query.over18;
+  var over18 = req.query.over18
 
-  if (over18 == "false"){
-
+  if (over18 == 'false') {
     // redirect to the relevant page
-    res.redirect("/examples/under-18");
-
+    res.redirect('/examples/under-18')
   } else {
-
     // if over18 is any other value (or is missing) render the page requested
-    res.render('examples/over-18');
-
+    res.render('examples/over-18')
   }
-
-});
+})
 
 // add your routes here
 
-router.get('/search', function(req, res){
-
+router.get('/search', function (req, res) {
   // todo:
   // get facets: mainstream browse, orgs, topics, format
 
-  var query = req.query;
+  var query = req.query
   var viewData = {
-    formatsQuery: "",
-    organisationsQuery:""
-  };
-
-  var start = (query.start == undefined) ? 0  : Number(query.start);
-  var count = (query.count == undefined) ? 30 : Number(query.count);
-  var organisations = query.organisations;
-  var formats = query.formats;
-
-  var queryObj = {};
-
-  queryObj.start = start;
-  queryObj.count = count;
-
-  if (organisations != undefined){
-    queryObj.filter_organisations = organisations;
-    viewData.formatsQuery = "&organisations=" + organisations;
+    formatsQuery: '',
+    organisationsQuery: ''
   }
 
-  if (formats != undefined){
-    queryObj.filter_format = formats;
-    viewData.formatsQuery = "&formats=" + formats;
+  var start = (query.start == undefined) ? 0 : Number(query.start)
+  var count = (query.count == undefined) ? 30 : Number(query.count)
+  var organisations = query.organisations
+  var formats = query.formats
+
+  var queryObj = {}
+
+  queryObj.start = start
+  queryObj.count = count
+
+  if (organisations != undefined) {
+    queryObj.filter_organisations = organisations
+    viewData.formatsQuery = '&organisations=' + organisations
   }
 
-  var query = querystring.stringify(queryObj);
+  if (formats != undefined) {
+    queryObj.filter_format = formats
+    viewData.formatsQuery = '&formats=' + formats
+  }
 
-  var govukSearchURL = "https://www.gov.uk/api/search.json?" + query;
+  var query = querystring.stringify(queryObj)
 
-  console.log(govukSearchURL);
+  var govukSearchURL = 'https://www.gov.uk/api/search.json?' + query
+
+  console.log(govukSearchURL)
 
   request(govukSearchURL)
-  .then(function(searchResponse){
-
-    var searchData = JSON.parse(searchResponse);
-    viewData.results = searchData.results;
-    viewData.totalResults = numberToCommas(searchData.total);
+  .then(function (searchResponse) {
+    var searchData = JSON.parse(searchResponse)
+    viewData.results = searchData.results
+    viewData.totalResults = numberToCommas(searchData.total)
 
     // log(viewData.results);
 
-    return request("https://www.gov.uk/api/search.json?facet_format=1000&count=0");
-
-  }).then(function(formatsResponse){
-
-    var formatsData = JSON.parse(formatsResponse);
+    return request('https://www.gov.uk/api/search.json?facet_format=1000&count=0')
+  }).then(function (formatsResponse) {
+    var formatsData = JSON.parse(formatsResponse)
 
     // log(formatsData);
 
-    viewData.formats = formatsData.facets.format.options.map(function(facet){
+    viewData.formats = formatsData.facets.format.options.map(function (facet) {
       return {
-        'name' : facet.value.slug.replace(/_/g, ' '),
-        'slug' : facet.value.slug,
+        'name': facet.value.slug.replace(/_/g, ' '),
+        'slug': facet.value.slug,
         'count': facet.documents
-      };
-    });
-
-    viewData.formats.sort(function(a,b){
-      if (a.name > b.name){
-        return 1;
-      } else {
-        return -1;
       }
-    });
+    })
 
-    return request("https://www.gov.uk/api/search.json?facet_organisations=1000&count=0");
+    viewData.formats.sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1
+      } else {
+        return -1
+      }
+    })
 
-  }).then(function(organisationsResponse){
+    return request('https://www.gov.uk/api/search.json?facet_organisations=1000&count=0')
+  }).then(function (organisationsResponse) {
+    var organisationsData = JSON.parse(organisationsResponse)
 
-    var organisationsData = JSON.parse(organisationsResponse);
+    log(organisationsData)
 
-    log(organisationsData);
-
-    viewData.organisations = organisationsData.facets.organisations.options.map(function(facet){
-      if (facet.value.title){
+    viewData.organisations = organisationsData.facets.organisations.options.map(function (facet) {
+      if (facet.value.title) {
         return {
-          'name' : facet.value.title,
-          'slug' : facet.value.slug,
+          'name': facet.value.title,
+          'slug': facet.value.slug,
           'count': facet.documents
-        };
+        }
       }
-    });
+    })
 
-    viewData.organisations.sort(function(a,b){
-      if (a.name > b.name){
-        return 1;
+    viewData.organisations.sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1
       } else {
-        return -1;
+        return -1
       }
-    });
+    })
 
-    viewData.start = start;
-    viewData.count = count;
-    viewData.nextPage = start + count;
-    viewData.prevPage = start - count;
-    res.render('search', viewData);
+    viewData.start = start
+    viewData.count = count
+    viewData.nextPage = start + count
+    viewData.prevPage = start - count
+    res.render('search', viewData)
+  })
+})
 
-  });
-
-});
-
-
-module.exports = router;
+module.exports = router
